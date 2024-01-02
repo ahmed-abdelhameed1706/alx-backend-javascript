d92e38d045de549e091240cb092457ddd39e09ad
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2024-01-02 10:00:34 +0200

    8-getBudgetCurrentYear.js

---
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -0,0 +1,20 @@
+function getCurrentYear() {
+    const date = new Date();
+    return date.getFullYear();
+  }
+  
+  export default function getBudgetForCurrentYear(income, gdp, capita) {
+    const budget = {
+        [`income-${getCurrentYear()}`]: income,
+        [`gdp-${getCurrentYear()}`]: gdp,
+        [`capita-${getCurrentYear()}`]: capita,
+    };
+  
+    return budget;
+  }
+
+//   const budget = {};
+  
+//     budget[`income-${getCurrentYear()}`] = income;
+//     budget[`gdp-${getCurrentYear()}`] = gdp;
+//     budget[`capita-${getCurrentYear()}`] = capita;
