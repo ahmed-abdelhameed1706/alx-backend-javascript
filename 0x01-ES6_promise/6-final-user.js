0c39a7aa482cbe77df14a0f359e6e12a15724900
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2024-01-03 16:25:58 +0200

    6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,6 +5,6 @@ export default async function handleProfileSignup(firstName, lastName, fileName)
   return Promise.allSettled([signUpUser(firstName, lastName),
     uploadPhoto(fileName)]).then((results) => results.map((result) => ({
     status: result.status,
-    value: result.value === 'fulfilled' ? result.value : String(result.reason),
+    value: result.status === 'fulfilled' ? result.value : String(result.reason),
   })));
 }
