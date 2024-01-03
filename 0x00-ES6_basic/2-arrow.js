3e29b7d7277642a6a007078db14bfa62091c85aa
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2024-01-03 10:23:07 +0200

    adding files

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,13 +1,12 @@
 export default function getNeighborhoodsList() {
-    this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
-  
-    const self = this;
-    this.addNeighborhood = (newNeighborhood) => {
-        self.sanFranciscoNeighborhoods.push(newNeighborhood);
-        return self.sanFranciscoNeighborhoods;
-        };
-  }
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
+  const self = this;
+  this.addNeighborhood = (newNeighborhood) => {
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
+  };
+}
 
 //   this.addNeighborhood = function add(newNeighborhood) {
 //     self.sanFranciscoNeighborhoods.push(newNeighborhood);
