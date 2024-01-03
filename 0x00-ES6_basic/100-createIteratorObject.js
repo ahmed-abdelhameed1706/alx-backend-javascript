d38bdeb8627a41a4f937219e6a0fc2506c0b9e8e
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2024-01-03 10:42:17 +0200

    100-createIteratorObject.js

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,10 +1,12 @@
 export default function createIteratorObject(report) {
-    const employeesNames = []
-    const employees = report.allEmployees
+  const employeesNames = [];
+  const employees = report.allEmployees;
 
-    for (let departmentName in employees) {
-        employeesNames.push(...[...employees[departmentName]])
+  for (const departmentName in employees) {
+    if (Object.prototype.hasOwnProperty.call(employees, departmentName)) {
+      employeesNames.push(...employees[departmentName]);
     }
+  }
 
-    return employeesNames
+  return employeesNames;
 }
