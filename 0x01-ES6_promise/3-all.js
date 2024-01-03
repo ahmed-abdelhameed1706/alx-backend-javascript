6c02ba94ddd475248ec393c5b9044628b384bef1
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2024-01-03 15:44:13 +0200

    3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()]).then(([photo, user]) => {
+  return Promise.all([uploadPhoto(), createUser()]).then(([photo, user]) => {
     console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
   }).catch(() => console.log('Signup system offline'));
 }
