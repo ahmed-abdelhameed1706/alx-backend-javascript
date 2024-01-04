190e8e7abc639674aecf02afe16ccc0348cd6854
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2024-01-04 13:09:21 +0200

    2-hbtn_course.js

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,40 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(val) {
+    if (typeof (val) !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = val;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(val) {
+    if (typeof (val) !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = val;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(val) {
+    if (!Array.isArray(val)) {
+      throw new TypeError('Students must be an array');
+    }
+    this._students = val;
+  }
+}
