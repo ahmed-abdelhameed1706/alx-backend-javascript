be790bebab8f4da4f85b64eb36bc6dfc2dd800ba
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2024-01-04 19:14:54 +0200

    fixing

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -3,8 +3,8 @@ import Currency from './3-currency';
 
 export default class Pricing {
   constructor(amount, currency) {
-    this._amount = amount;
-    this._currency = currency;
+    this.amount = amount;
+    this.currency = currency;
   }
 
   get amount() {
--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -3,7 +3,7 @@ export default class Building {
     if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
-    this._sqft = sqft;
+    this.sqft = sqft;
   }
 
   get sqft() {
