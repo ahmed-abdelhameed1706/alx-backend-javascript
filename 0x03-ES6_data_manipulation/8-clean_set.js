011840daf7e0420752b267dd38100e5282ea3353
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2024-01-09 15:39:30 +0200

    8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,8 @@
 export default function cleanSet(set, startString) {
+  if (!(set instanceof Set) || typeof startString !== 'string' || !set || !startString) {
+    return '';
+  }
+
   return Array.from(set)
     .filter((element) => (startString.length > 0 ? element.startsWith(startString) : null))
     .map((filteredElement) => filteredElement.slice(startString.length)).join('-');
