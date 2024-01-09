2d9c90138dcd3181f30e6d3a1d3ed6cf6fe6aab4
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2024-01-09 15:54:10 +0200

    10-update_uniq_items.js

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,4 +1,8 @@
 export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+
   for (const [key, value] of map.entries()) {
     if (value === 1) {
       map.set(key, 100);
