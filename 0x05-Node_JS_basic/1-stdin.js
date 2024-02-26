ca1fcaaf801b3fee6071e53f77c98446f34d06ff
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2024-02-26 21:14:16 +0200

    1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,7 +1,7 @@
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
-process.stdin.on("data", (data) => {
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.on('data', (data) => {
   process.stdout.write(`Your name is: ${data}`);
 });
-process.stdin.on("end", () => {
-  process.stdout.write("This important software is now closing\n");
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
