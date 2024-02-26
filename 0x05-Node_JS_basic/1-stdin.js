0b1f760bbf37eb8a9658b1d637746d6549b741d3
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2024-02-26 21:12:41 +0200

    1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,15 +1,7 @@
-const readline = require("readline");
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+process.stdout.write("Welcome to Holberton School, what is your name?\n");
+process.stdin.on("data", (data) => {
+  process.stdout.write(`Your name is: ${data}`);
 });
-
-rl.question("Welcome to Holberton School, what is your name?\n", (name) => {
-  console.log(`Your name is: ${name}!`);
-  rl.close();
-});
-
-rl.on("close", () => {
-  console.log("This important software is now closing");
+process.stdin.on("end", () => {
+  process.stdout.write("This important software is now closing\n");
 });
