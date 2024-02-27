f81ef2ff6e1419565b12344f8737b3fdda1d56d7
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2024-02-27 21:59:46 +0200

    3-read_file_async.js

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+
+const countStudents = (path) => new Promise((resolve, reject) => {
+  let numberOfStudents = 0;
+  const studentsInField = {};
+
+  fs.readFile(path, 'utf8', (err, data) => {
+    if (err) {
+      reject(new Error('Cannot load the database'));
+      return;
+    }
+
+    const rows = data.split('\n');
+
+    for (let i = 1; i < rows.length; i += 1) {
+      const row = rows[i].trim();
+
+      if (row !== '') {
+        numberOfStudents += 1;
+
+        const columns = row.split(',');
+        const firstName = columns[0];
+        const field = columns[columns.length - 1];
+
+        if (Object.prototype.hasOwnProperty.call(studentsInField, field)) {
+          studentsInField[field].push(firstName);
+        } else {
+          studentsInField[field] = [firstName];
+        }
+      }
+    }
+
+    console.log(`Number of students: ${numberOfStudents}`);
+    for (const key of Object.keys(studentsInField)) {
+      if (Object.prototype.hasOwnProperty.call(studentsInField, key)) {
+        console.log(
+          `Number of students in ${key}: ${
+            studentsInField[key].length
+          }. List: ${studentsInField[key].join(', ')}`,
+        );
+      }
+    }
+
+    resolve({ numberOfStudents, studentsInField });
+  });
+});
+
+module.exports = countStudents;
